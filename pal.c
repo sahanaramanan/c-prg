fcc9c9e9f503fbafc566d71d039319ae421f368f
sahanaramanan <47449689+sahanaramanan@users.noreply.github.com>
2019-02-10 12:09:15 +0530

    Update pal.c

--- pal.c
+++ pal.c
@@ -5,15 +5,15 @@ int main()
    printf("Enter an integer to check if it is palindrome or not\n");
    scanf("%d", &number);
    total = number;
-   while (total != 0)
+   while (total <=100)
    {
       reminder = reminder * 10;
-      reminder = reminder + t%10;
+      reminder = reminder + total%10;
       total = total/10;
    }
    if (number == reminder)
-      printf("%d is a palindrome number.\n", number);
+      printf("%d  palindrome number.\n", number);
    else
-      printf("%d isn't a palindrome number.\n", number);
+      printf("%d not palindrome number.\n", number);
    return 0;
 }
