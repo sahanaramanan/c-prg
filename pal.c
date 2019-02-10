4655d90d508be73745d7bed7a1aedd01c37f66d7
sahanaramanan <47449689+sahanaramanan@users.noreply.github.com>
2019-02-10 12:00:22 +0530

    Update and rename README.md to pal.c

---
+++ pal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int number, reminder = 0, total;
+   printf("Enter an integer to check if it is palindrome or not\n");
+   scanf("%d", &n);
+   total = number;
+   while (total != 0)
+   {
+      reminder = reminder * 10;
+      reminder = reminder + t%10;
+      total = total/10;
+   }
+   if (number == reminder)
+      printf("%d is a palindrome number.\n", number);
+   else
+      printf("%d isn't a palindrome number.\n", number);
+   return 0;
+}
